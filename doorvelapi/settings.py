4cf7667d302267ead03e64f1f65a140a84b4f9dc
AaronARdz <aaron_acostarz@outlook.es>
2023-02-19 13:17:22 -0600

    added mysql connection

--- doorvelapi/settings.py
+++ doorvelapi/settings.py
@@ -78,10 +78,21 @@ WSGI_APPLICATION = 'doorvelapi.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'newdb.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'newdb.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'django',
+        'USER': 'admin',
+        'PASSWORD': '',
+        'HOST': 'django-test.cpdjtxghqcxh.us-east-2.rds.amazonaws.com',
+        'PORT': '3306',
     }
 }
 
